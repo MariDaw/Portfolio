e6788827b53097b4686e9d611b35cc8a8b7ec51b
DAW <maria.quiros@iesdonana.org>
2022-03-03 19:41:26 +0100

    arreglando

--- js/dialog.js
+++
@@ -1,8 +0,0 @@
-const dialog = document.querySelector('#dialogo-1');
-dialogPolyfill.registerDialog(dialog);
-document.querySelector('#show-modal').onclick = function() {
-  dialog.showModal();
-};
-document.querySelector('#close').onclick = function() {
-  dialog.close();
-};
